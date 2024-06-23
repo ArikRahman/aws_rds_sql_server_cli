f23ee8747cfb95381cb24226131f202a70d2956f
arik <arik@tamu.edu>
2024-06-23 01:28:23 -0500

    init

--- obj/Debug/net9.0/aws_rds_sql_server_cli.AssemblyInfo.cs
+++ obj/Debug/net9.0/aws_rds_sql_server_cli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aws_rds_sql_server_cli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10bd407055e02b031ade06f17ecf78d41e2ad35c")]
 [assembly: System.Reflection.AssemblyProductAttribute("aws_rds_sql_server_cli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aws_rds_sql_server_cli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
